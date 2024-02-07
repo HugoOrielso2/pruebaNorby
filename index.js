674209307916b49ad954cf636033376394d6e949
Hugo Orielso <orielso.lozano15@gmail.com>
2024-02-07 15:45:13 -0500

    changes modules

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ import express from 'express'
 
 
 const app = express()
+const PORT = process.env.PORT ?? 4321
+
 app.use("/", express.static('dist', {redirect:false}))
-app.get("/pruebaTres", (req,res)=>{
-    return res.status(200).json({
-        status: "Bien",
-        message: "Deploy"
-    })
-})
 app.get("*", (req,res,next)=>{
     res.sendFile(process.cwd() + '/dist/index.html')
 })
 
-
-
-const PORT = process.env.PORT ?? 4321
 
 app.listen(PORT,()=>{
     console.log(`Server on port ${PORT}`);
